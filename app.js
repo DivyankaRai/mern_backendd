5af3f043ca6119f4ab117407947cfed55bd58de6
divyanka05 <divyanka@cashflowy.io>
2024-06-24 15:36:21 +0530

    made some changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('./db/db')
 
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:30000',
     credentials: true 
 }));
 app.use(express.json())
